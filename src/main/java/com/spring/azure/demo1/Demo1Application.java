08ca802287334ede6063a6ce8e22d6b2699fc333
iamsurajitmishra <mishrasurajit69@gmail.com>
2022-06-14 02:00:08 +0530

    Initial commit

---
+++ src/main/java/com/spring/azure/demo1/Demo1Application.java
@@ -0,0 +1,20 @@
+package com.spring.azure.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo1Application {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats!! Your application deployed to azure!!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
